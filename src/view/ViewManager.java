5a63cae6f362c6ff014574b4846f350d9af49300
arielkhait <arielkh03@yahoo.com>
2023-10-23 12:12:34 -0400

    started a view manager

--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -1,4 +1,25 @@
 package view;
 
-public class ViewManager {
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class ViewManager implements PropertyChangeListener{
+    private final CardLayout cardLayout;
+    private final JPanel views;
+
+    private ViewManagerModel viewManagerModel; //from the interface adapter
+
+    public ViewManager(JPanel views, CardLayout cardLayout, ViewManagerModel viewManagerModel) {
+        this.views = views;
+        this.cardLayout = cardLayout;
+        this.viewManagerModel = viewManagerModel;
+        this.viewManagerModel.addPropertyChangeListener(this);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        // to impliment later
+    }
 }
