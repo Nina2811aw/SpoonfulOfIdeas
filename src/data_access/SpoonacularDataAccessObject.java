119cf84d7b2e43f0653ec8f0e2a8f44f846e9e0b
arielkhait <arielkh03@yahoo.com>
2023-10-23 11:59:18 -0400

    added import statements

--- src/data_access/SpoonacularDataAccessObject.java
+++ src/data_access/SpoonacularDataAccessObject.java
@@ -1,4 +1,5 @@
 package data_access;
 
 public class SpoonacularDataAccessObject {
+
 }
--- src/view/RecipeSearchView.java
+++ src/view/RecipeSearchView.java
@@ -1,4 +1,13 @@
 package view;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class RecipeSearchView {
+
 }
