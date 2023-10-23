b0cfe65ae304d890983c4b91c35c76c695bb91c0
Nina2811aw <anietta.weckauff.mail@utoronto.ca>
2023-10-23 12:09:51 -0400

    created classes

---
+++ src/interface_adapter/choose_recipe/ChooseRecipeController.java
@@ -0,0 +1,4 @@
+package interface_adapter.choose_recipe;
+
+public class ChooseRecipeController {
+}
--- src/use_case/recipe_search/RecipeSearchInteractor.java
+++ src/use_case/recipe_search/RecipeSearchInteractor.java
@@ -1,4 +1,4 @@
-package use_case;
+package use_case.recipe_search;
 
 public class RecipeSearchInteractor {
 }
