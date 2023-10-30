83c893da9607719417a0c440f11c778204ab755a
arielkhait <arielkh03@yahoo.com>
2023-10-30 12:13:49 -0400

    Created First View Page

--- src/interface_adapter/choose_recipe/ChooseRecipeViewModel.java
+++ src/interface_adapter/choose_recipe/ChooseRecipeViewModel.java
@@ -1,4 +1,35 @@
 package interface_adapter.choose_recipe;
 
-public class ChooseRecipeViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class ChooseRecipeViewModel extends ViewModel {
+    public static final String SELECT_BUTTON = "Select";
+    private ChooseRecipeState state = new ChooseRecipeState();
+
+    public ChooseRecipeViewModel() {
+        super("Choose Recipe");
+    }
+
+    public void setState(ChooseRecipeState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the Signup Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public ChooseRecipeState getState() {
+        return state;
+    }
 }
--- src/view/ChooseRecipeView.java
+++ src/view/ChooseRecipeView.java
@@ -4,6 +4,7 @@ import interface_adapter.choose_recipe.ChooseRecipeController;
 import interface_adapter.choose_recipe.ChooseRecipeViewModel;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -17,9 +18,19 @@ public class ChooseRecipeView extends JPanel implements ActionListener, Property
 
     public final ChooseRecipeController chooseRecipeController;
 
+    final JButton select;
+
     public ChooseRecipeView(ChooseRecipeController chooseRecipeController, ChooseRecipeViewModel chooseRecipeViewModel){
         this.chooseRecipeController = chooseRecipeController;
         this.chooseRecipeViewModel = chooseRecipeViewModel;
+        this.chooseRecipeViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel("Choose Recipe Screen");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel buttons = new JPanel();
+        select = new JButton(chooseRecipeViewModel.SELECT_BUTTON);
+        buttons.add(select);
     }
 
 
