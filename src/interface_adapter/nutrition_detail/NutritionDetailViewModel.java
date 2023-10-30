b2dff52bca6ffce6eb1a2d8e9a35f9c37a36bb08
arielkhait <arielkh03@yahoo.com>
2023-10-30 12:30:53 -0400

    Created a Recipe Details View Model and View

--- src/interface_adapter/nutrition_detail/NutritionDetailViewModel.java
+++ src/interface_adapter/nutrition_detail/NutritionDetailViewModel.java
@@ -1,4 +1,39 @@
 package interface_adapter.nutrition_detail;
 
-public class NutritionDetailViewModel {
+import interface_adapter.ViewModel;
+import interface_adapter.recipe_search.RecipeSearchState;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class NutritionDetailViewModel extends ViewModel {
+    public static final String TITLE_LABEL = "Recipe Search View";
+
+    public static final String SEARCH_BUTTON = "Search";
+
+    private RecipeSearchState state = new RecipeSearchState();
+
+    public NutritionDetailViewModel() {
+        super("search");
+    }
+
+    public void setState(RecipeSearchState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the Signup Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public RecipeSearchState getState() {
+        return state;
+    }
 }
--- src/view/RecipeDetailsView.java
+++ src/view/RecipeDetailsView.java
@@ -1,4 +1,28 @@
 package view;
 
+import interface_adapter.recipe_search.RecipeSearchController;
+
+import javax.swing.*;
+import java.awt.*;
+
+
 public class RecipeDetailsView {
+
+    private final RecipeDetailsController recipeDetailsController;
+
+    private final RecipeDetailsViewModel recipeViewModel;
+    final JButton back;
+    public RecipeDetailsView(RecipeDetailsViewModel recipeDetailsViewModel, RecipeDetailsController controller) {
+
+        this.recipeDetailsController = controller;
+        this.recipeViewModel = recipeDetailsViewModel;
+        this.recipeViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel("Recipe Details View");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+        JPanel buttons = new JPanel();
+        back = new JButton(recipeViewModel.BACK_BUTTON_LABEL);
+        buttons.add(back);
 }
