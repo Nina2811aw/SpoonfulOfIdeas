53761062ee1b8b84b1a8eafa61b16524bff97394
anon <75766435+anon1192@users.noreply.github.com>
2023-10-30 12:56:46 -0400

    merge commit

--- src/interface_adapter/nutrition_detail/NutritionDetailController.java
+++ src/interface_adapter/nutrition_detail/NutritionDetailController.java
@@ -1,4 +1,18 @@
 package interface_adapter.nutrition_detail;
 
+import use_case.choose_recipe.NutritionDetailInputBoundary;
+import use_case.choose_recipe.NutritionDetailInputData;
+
 public class NutritionDetailController {
+    final NutritionDetailInputBoundary nutritionDetailInputInteractor;
+
+
+    public NutritionDetailController(NutritionDetailInputBoundary nutritionDetailInputInteractor) {
+        this.NutritionDetailInputInteractor = nutritionDetailInputInteractor;
+    }
+
+    public void execute(){
+        NutritionDetailInputData nutritionDetailInputData = null;
+        this.NutritionDetailInputInteractor.execute(nutritionDetailInputData);
+    }
 }
