4ccd06cd29055c46c52951fc58df476e8f7356b4
anon <75766435+anon1192@users.noreply.github.com>
2023-10-30 12:17:20 -0400

    merge commit

--- src/data_access/SpoonacularDataAccessObject.java
+++ src/data_access/SpoonacularDataAccessObject.java
@@ -6,9 +6,6 @@ import use_case.recipe_search.RecipeSearchDataAccessInterface;
 import use_case.recipe_search.RecipeSearchInputData;
 
 public class SpoonacularDataAccessObject implements RecipeSearchDataAccessInterface, ChooseRecipeDataAccessInterface {
-
-    // we have to think about what exactly this method will return after having made the API call
-    // the return type void has to be adapted adequately
     @Override
     public void getRecipeIdeas(RecipeSearchInputData recipeSearchInputData) {
 
@@ -16,7 +13,44 @@ public class SpoonacularDataAccessObject implements RecipeSearchDataAccessInterf
     }
 
     @Override
-    public void getRecipeDetails(ChooseRecipeInputData chooseRecipeInputData) {
+    public void getRecipeDetails(ChooseRecipeInputData chooseRecipeInputData)     {
+        String API_TOKEN = "47e1335f069c4ff1b2fbb1ea17cf2179";
 
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        for (int i = 0; i < recipeId.size(); i++) {
+            String id = String.valueOf(recipeId.get(i));
+            String url = "https://api.spoonacular.com/recipes/" + id + "/information?includeNutrition=false";
+            Request request = new Request.Builder()
+                    .url(url)
+                    .addHeader("x-api-key", API_TOKEN)
+                    .addHeader("Content-Type", "application/json")
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                assert response.body() != null;
+                JSONObject responseBody = new JSONObject(response.body().string());
+                List<String> inner_lst = new ArrayList<>();
+
+
+                // Adding id to nested list
+                inner_lst.add(id);
+
+
+                // Adding title to nested list
+                inner_lst.add(responseBody.get("title").toString());
+
+                // Adding Instructions to the list
+                inner_lst.add(responseBody.get("instructions").toString());
+                info_list.add(inner_lst);
+                //System.out.println(responseBody);
+                //System.out.println(responseBody.get("title").toString());
+                //System.out.println("Cooking Time: " + responseBody.get("title").toString() + " min");
+                //System.out.println(responseBody.get("instructions").toString());
+
+            } catch (IOException | JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
