397c9289a1584a1fc5f3966ed4063db22ea5217c
Nina2811aw <anietta.weckauff.mail@utoronto.ca>
2023-11-02 11:47:04 -0400

    adapted classes of the nutrition detail use case to remove errors

--- src/interface_adapter/nutrition_detail/NutritionDetailController.java
+++ src/interface_adapter/nutrition_detail/NutritionDetailController.java
@@ -1,18 +1,22 @@
 package interface_adapter.nutrition_detail;
 
-import use_case.choose_recipe.NutritionDetailInputBoundary;
-import use_case.choose_recipe.NutritionDetailInputData;
+import use_case.nutrition_detail.NutritionDetailInputBoundary;
+import use_case.nutrition_detail.NutritionDetailInputData;
 
 public class NutritionDetailController {
-    final NutritionDetailInputBoundary nutritionDetailInputInteractor;
+    //final NutritionDetailInputBoundary nutritionDetailInputInteractor;
+
+    final NutritionDetailInputBoundary nutritionDetailInteractor;
 
 
-    public NutritionDetailController(NutritionDetailInputBoundary nutritionDetailInputInteractor) {
-        this.NutritionDetailInputInteractor = nutritionDetailInputInteractor;
+    public NutritionDetailController(NutritionDetailInputBoundary nutritionDetailInteractor) {
+        this.nutritionDetailInteractor = nutritionDetailInteractor;
     }
 
     public void execute(){
-        NutritionDetailInputData nutritionDetailInputData = null;
-        this.NutritionDetailInputInteractor.execute(nutritionDetailInputData);
+        // constructor call has to be adapted dependent on the attributes of the input data
+        NutritionDetailInputData nutritionDetailInputData = new NutritionDetailInputData();
+
+        this.nutritionDetailInteractor.execute(nutritionDetailInputData);
     }
 }
