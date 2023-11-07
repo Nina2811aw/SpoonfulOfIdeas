4d75282ae20fc99a521459f72bc4695e46e69a4a
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-11-07 18:16:09 -0500

    Create setters and getters methods in RecipeSpec interface

--- src/entity/RecipeSpec.java
+++ src/entity/RecipeSpec.java
@@ -1,4 +1,54 @@
 package entity;
 
-public interface RecipeSpec {
+import use_case.recipe_search.RecipeSearchInputData;
+
+interface RecipeSpec {
+
+    // User specification bundled data
+    RecipeSearchInputData getSpecification();
+    void setSpecification(RecipeSearchInputData recipeSearchInputData);
+
+    // Ingredients
+    String getIngredients();
+    void setIngredients(String ingredients);
+
+    // Diets
+    String getDiets();
+    void setDiets(String diets);
+
+    // Cuisines
+    String getCuisines();
+    void setCuisines(String cuisines);
+
+    // Allergies
+    String getAllergies();
+    void setAllergies(String allergies);
+
+    // Protein
+    int getMinProtein();
+    void setMinProtein(int minProtein);
+
+    int getMaxProtein();
+    void setMaxProtein(int maxProtein);
+
+    // Fat
+    int getMinFat();
+    void setMinFat(int minFat);
+
+    int getMaxFat();
+    void setMaxFat(int maxFat);
+
+    // Carbs
+    int getMinCarbs();
+    void setMinCarbs(int minCarbs);
+
+    int getMaxCarbs();
+    void setMaxCarbs(int maxCarbs);
+
+    // Calories
+    int getMinCalories();
+    void setMinCalories(int minCalories);
+
+    int getMaxCalories();
+    void setMaxCalories(int maxCalories);
 }
