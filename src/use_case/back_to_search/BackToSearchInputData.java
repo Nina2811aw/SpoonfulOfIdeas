c83bdf29bc09f69b6c544ef96800fceb37fd705f
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-11-07 21:37:00 -0500

    Outline back_to_search interactor + implement constructor

--- src/use_case/back_to_search/BackToSearchInputData.java
+++ src/use_case/back_to_search/BackToSearchInputData.java
@@ -1,4 +1,5 @@
 package use_case.back_to_search;
 
+// This is an empty class as the BackToSearch use case does not utilize any input data.
 public class BackToSearchInputData {
 }
--- src/use_case/back_to_search/BackToSearchInteractor.java
+++ src/use_case/back_to_search/BackToSearchInteractor.java
@@ -1,4 +1,24 @@
 package use_case.back_to_search;
 
-public class BackToSearchInteractor {
+import data_access.RecipeSpecificationDataAccessObject;
+import entity.RecipeSpec;
+
+public class BackToSearchInteractor implements BackToSearchInputBoundary {
+
+    private BackToSearchDataAccessInterface recipeSpecificationDAO;
+    private BackToSearchOutputBoundary backToSearchOutputBoundary;
+
+    //    private BackToSearchOutputData backToSearchOutputData;
+    //    private RecipeSpec recipeSpecification;
+
+    public BackToSearchInteractor(BackToSearchDataAccessInterface backToSearchDataAccessInterface,
+                                  BackToSearchOutputBoundary backToSearchOutputBoundary){
+        this.recipeSpecificationDAO = backToSearchDataAccessInterface;
+        this.backToSearchOutputBoundary = backToSearchOutputBoundary;
+    }
+
+    @Override
+    public void execute() {
+
+    }
 }
