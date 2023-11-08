fa8f7a36d14282f108997851874548e818fabacf
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-11-07 19:23:12 -0500

    Implement BackToSearch use case object + DAO

--- src/use_case/back_to_search/BackToSearchDataAccessInterface.java
+++ src/use_case/back_to_search/BackToSearchDataAccessInterface.java
@@ -1,4 +1,13 @@
 package use_case.back_to_search;
 
+import entity.RecipeSpec;
+import use_case.recipe_search.RecipeSearchInputData;
+
 public interface BackToSearchDataAccessInterface {
+
+    // for BackToSearch use case interactor to access user specifications.
+    RecipeSpec getSpecification();
+
+    // for SearchRecipe use case interactor to set user specifications input data into entity object.
+    void setSpecification(RecipeSearchInputData recipeSearchInputData);
 }
