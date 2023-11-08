ca755b2994378c3774883c2f7a6138201a648e73
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-11-07 21:20:04 -0500

    Implement BackToSearch Controller and InputBoundary

--- src/interface_adapter/back_to_search/BackToSearchController.java
+++ src/interface_adapter/back_to_search/BackToSearchController.java
@@ -1,4 +1,16 @@
 package interface_adapter.back_to_search;
 
+import use_case.back_to_search.BackToSearchInputBoundary;
+
 public class BackToSearchController {
+
+    final BackToSearchInputBoundary backToSearchInputBoundary;
+
+    public BackToSearchController(BackToSearchInputBoundary backToSearchInteractor){
+        this.backToSearchInputBoundary = backToSearchInteractor;
+    }
+
+    public void execute(){
+        backToSearchInputBoundary.execute();
+    }
 }
