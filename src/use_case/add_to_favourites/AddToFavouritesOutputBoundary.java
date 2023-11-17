34dc608c3c1d5e882c5cc1766d5e0c578b7e9d64
shreya <shreya.dubey@mail.utoronto.ca>
2023-11-17 17:24:56 -0500

    Implemented the DAO for this use case. This included creating a recipes HashMap that stores the favourited recipes. Wrote methods that write the recipe id and title to the file, along with a method that gets the titles of all the favourited recipes for the success view.

--- src/use_case/add_to_favourites/AddToFavouritesOutputBoundary.java
+++ src/use_case/add_to_favourites/AddToFavouritesOutputBoundary.java
@@ -1,4 +1,6 @@
 package use_case.add_to_favourites;
 
 public interface AddToFavouritesOutputBoundary {
+    void prepareSuccessView(AddToFavouritesOutputData output);
+    void prepareFailView(String error);
 }
