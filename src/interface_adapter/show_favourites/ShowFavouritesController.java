a004d5c173683f750bb6187fe8a0f977352b0123
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-11-19 18:08:58 -0500

    Implement show-favourites controller class

--- src/interface_adapter/show_favourites/ShowFavouritesController.java
+++ src/interface_adapter/show_favourites/ShowFavouritesController.java
@@ -1,2 +1,15 @@
-package interface_adapter.show_favourites;public class ShowFavouritesController {
+package interface_adapter.show_favourites;
+
+import use_case.show_favourites.ShowFavouritesInputBoundary;
+
+public class ShowFavouritesController {
+
+    final private ShowFavouritesInputBoundary showFavouritesInteractor;
+    public ShowFavouritesController(ShowFavouritesInputBoundary showFavouritesInteractor){
+        this.showFavouritesInteractor = showFavouritesInteractor;
+    }
+
+    public void execute(){
+        showFavouritesInteractor.execute();
+    }
 }
