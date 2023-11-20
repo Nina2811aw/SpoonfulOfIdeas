5099b8f95bbd663e45bb555fa9c33cae86797a01
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-11-19 19:38:49 -0500

    Implement show-favourites use case interactor

--- src/use_case/show_favourites/ShowFavouritesInteractor.java
+++ src/use_case/show_favourites/ShowFavouritesInteractor.java
@@ -1,2 +1,19 @@
-package use_case.show_favourites;public class ShowFavouritesInteractor {
+package use_case.show_favourites;
+
+import use_case.recipe_search.RecipeSearchOutputBoundary;
+import use_case.recipe_search.RecipeSearchOutputData;
+
+public class ShowFavouritesInteractor implements ShowFavouritesInputBoundary {
+    final private ShowFavouritesDataAccessInterface favouritesDAO;
+    final private RecipeSearchOutputBoundary presenter;
+
+    public ShowFavouritesInteractor(ShowFavouritesDataAccessInterface favouritesDAO, RecipeSearchOutputBoundary presenter){
+        this.favouritesDAO = favouritesDAO;
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void execute() {
+        presenter.prepareChooseRecipeView(new RecipeSearchOutputData(favouritesDAO.getFavourites()));
+    }
 }
