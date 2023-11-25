f41c467c9c63b24da54d90b0a8ec841e2f20d687
anon <75766435+anon1192@users.noreply.github.com>
2023-11-24 22:42:53 -0500

    Nutrition label viewer added, when clicking on the 'View Nutritional Information' an image pop-up appears with the information

--- src/view/RecipeDetailsView.java
+++ src/view/RecipeDetailsView.java
@@ -3,8 +3,6 @@ package view;
 import interface_adapter.add_to_favourites.AddToFavouritesController;
 import interface_adapter.add_to_favourites.AddToFavouritesState;
 import interface_adapter.back_to_choose.BackToChooseController;
-import interface_adapter.back_to_choose.BackToChooseState;
-import interface_adapter.back_to_choose.BackToChooseViewModel;
 import interface_adapter.nutrition_detail.NutritionDetailController;
 import interface_adapter.nutrition_detail.NutritionDetailState;
 import interface_adapter.nutrition_detail.NutritionDetailViewModel;
@@ -13,42 +11,54 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.List;
 
+import static data_access.SpoonacularDataAccessObject.displayNutritionLabelImage;
 
 public class RecipeDetailsView extends JPanel implements ActionListener, PropertyChangeListener {
 
     public String viewName = "Recipe Details";
+    public final NutritionDetailViewModel nutritionDetailViewModel;
 
-    private final NutritionDetailController nutritionDetailController;
-
-    private final NutritionDetailViewModel nutritionDetailViewModel;
-
-    private final BackToChooseController backToChooseController;
+    //initalize buttons back, addToFavourites, nutritionDetail
     final JButton back;
-
-    private final AddToFavouritesController addToFavouritesController;
+    public final BackToChooseController backToChooseController;
 
     private final JButton addToFavourites;
+    public final AddToFavouritesController addToFavouritesController;
+
+    private final JButton nutritionDetail;
+    public final NutritionDetailController nutritionDetailController;
+
     public RecipeDetailsView(NutritionDetailController nutritionDetailController, NutritionDetailViewModel nutritionDetailViewModel, BackToChooseController backToChooseController, AddToFavouritesController addToFavouritesController) {
-        this.nutritionDetailController = nutritionDetailController;
         this.nutritionDetailViewModel = nutritionDetailViewModel;
-        this.backToChooseController = backToChooseController;
-        this.addToFavouritesController = addToFavouritesController;
+        this.nutritionDetailController = nutritionDetailController;
         nutritionDetailViewModel.addPropertyChangeListener(this);
 
-        JLabel title = new JLabel("Recipe Details View");
-        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.backToChooseController = backToChooseController;
+        this.addToFavouritesController = addToFavouritesController;
 
+        // Change
+        JLabel title = new JLabel("---title----");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //^^
+
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+
+        // Button Setup
         JPanel buttons = new JPanel();
+        JPanel details = new JPanel();
         back = new JButton(NutritionDetailViewModel.BACK_BUTTON_LABEL);
         buttons.add(back);
         addToFavourites = new JButton(NutritionDetailViewModel.ADD_TO_FAVOURITES_LABEL);
         buttons.add(addToFavourites);
+
+        nutritionDetail = new JButton(NutritionDetailViewModel.NUTRITION_INFO_LABEL);
+        details.add(nutritionDetail);
 
         back.addActionListener(
                 new ActionListener() {
@@ -79,11 +89,29 @@ public class RecipeDetailsView extends JPanel implements ActionListener, Propert
                 }
         );
 
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.add(title);
-        this.add(buttons);
+
+        // Show nutritional information check
+
+        nutritionDetail.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (e.getSource().equals(nutritionDetail)){
+                            NutritionDetailState nutritionState = NutritionDetailViewModel.getState();
+                            List<String> recipe = nutritionState.getRecipe();
+                            String id = recipe.get(0);
+
+                            displayNutritionLabelImage(id);
+                        }
+                    }
+                }
+        );
+
         // add the recipe info later
 
+        this.add(title);
+        this.add(buttons);
+        this.add(details);
 
 
 }
