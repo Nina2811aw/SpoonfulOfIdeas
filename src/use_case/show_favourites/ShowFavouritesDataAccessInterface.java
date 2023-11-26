48ea5133364129e9f558540377d7bf0f498b6bd8
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-11-25 20:03:29 -0500

    Comment show-favourites public methods for application business rules files

--- src/use_case/show_favourites/ShowFavouritesDataAccessInterface.java
+++ src/use_case/show_favourites/ShowFavouritesDataAccessInterface.java
@@ -5,5 +5,7 @@ import entity.RecipeInformation;
 import java.util.List;
 
 public interface ShowFavouritesDataAccessInterface {
+    // The show_favourites use case requires the locally stored data contained in favourites.csv, thus to force
+    // a level consistency with how this data looks, we require this data as a list of RecipeInformation entity objects.
     List<RecipeInformation> getFavouritesList();
 }
--- src/use_case/show_favourites/ShowFavouritesInteractor.java
+++ src/use_case/show_favourites/ShowFavouritesInteractor.java
@@ -7,11 +7,14 @@ public class ShowFavouritesInteractor implements ShowFavouritesInputBoundary {
     final private ShowFavouritesDataAccessInterface favouritesDAO;
     final private RecipeSearchOutputBoundary presenter;
 
+    // constructor setting up access to presenter and the favouritesDAO.
     public ShowFavouritesInteractor(ShowFavouritesDataAccessInterface favouritesDAO, RecipeSearchOutputBoundary presenter){
         this.favouritesDAO = favouritesDAO;
         this.presenter = presenter;
     }
 
+    // This use case will always work, thus we simply send in the output data to the presenter success view.
+    // Note that we're simply reusing the presenter from recipe-search use case, thus we use the same output data object.
     @Override
     public void execute() {
         presenter.prepareChooseRecipeView(new RecipeSearchOutputData(favouritesDAO.getFavouritesList()));
