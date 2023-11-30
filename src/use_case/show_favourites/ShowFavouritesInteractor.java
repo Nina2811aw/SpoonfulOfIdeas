46456a4ef89fb13e9da46dcd5f9394171ef261c9
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-11-30 17:31:00 -0500

    Add javadocs for show-favourites use case + delete unused files

--- src/use_case/show_favourites/ShowFavouritesInteractor.java
+++ src/use_case/show_favourites/ShowFavouritesInteractor.java
@@ -7,14 +7,20 @@ public class ShowFavouritesInteractor implements ShowFavouritesInputBoundary {
     final private ShowFavouritesDataAccessInterface favouritesDAO;
     final private RecipeSearchOutputBoundary presenter;
 
-    // constructor setting up access to presenter and the favouritesDAO.
+    /**
+     * Constructor sets up interactor so that it can access the presenter and the favouritesDAO.
+     * @param favouritesDAO should contain the FavouritesDataAccessObject object.
+     * @param presenter should contain the RecipeSearchPresenter object.
+     */
     public ShowFavouritesInteractor(ShowFavouritesDataAccessInterface favouritesDAO, RecipeSearchOutputBoundary presenter){
         this.favouritesDAO = favouritesDAO;
         this.presenter = presenter;
     }
 
-    // This use case will always work, thus we simply send in the output data to the presenter success view.
-    // Note that we're simply reusing the presenter from recipe-search use case, thus we use the same output data object.
+    /**
+     * This use case will always work, thus we simply send in the output data to the presenter success view.
+     * Note that we're simply reusing the presenter from recipe-search use case, thus we use the same output data object.
+     */
     @Override
     public void execute() {
         presenter.prepareChooseRecipeView(new RecipeSearchOutputData(favouritesDAO.getFavouritesList()));
--- src/use_case/show_favourites/ShowFavouritesOutputData.java
+++
@@ -1,3 +0,0 @@
-package use_case.show_favourites;public class ShowFavouritesOutputData {
-    // This use case utilizes the RecipeSearch presenter and outputData, so this an unused class.
-}
