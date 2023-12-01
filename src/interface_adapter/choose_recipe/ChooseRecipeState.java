ab692e6c98f67a1f503c9e91b998361d20097406
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-11-30 22:47:40 -0500

    Add simple javadocs for choose-recipe use case classes + minor formatting fixes

--- src/interface_adapter/choose_recipe/ChooseRecipeState.java
+++ src/interface_adapter/choose_recipe/ChooseRecipeState.java
@@ -17,6 +17,9 @@ public class ChooseRecipeState {
     private JButton button4;
     private JButton button5;
 
+    /**
+     * Constructs a new ChooseRecipeState with default recipe information (recipeId, recipeName).
+     */
     public ChooseRecipeState() {
         recipeIdeasList = new ArrayList<>();
         recipeIdeasList.add(new RecipeInformation(1,"hello1"));
@@ -24,11 +27,18 @@ public class ChooseRecipeState {
         recipeIdeasList.add(new RecipeInformation(3,"hello3"));
         recipeIdeasList.add(new RecipeInformation(4,"hello4"));
         recipeIdeasList.add(new RecipeInformation(5,"hello5"));
-
     }
 
+    /**
+     * Getter which returns the list of recipe information.
+     * @return the list of RecipeInformation objects
+     */
     public List<RecipeInformation> get_recipe_info(){return recipeIdeasList;}
 
+    /**
+     * Getter which returns a new array containing just the names of all recipes in the recipe ideas list.
+     * @return a list of recipe names
+     */
     public List<String> getRecipeNames() {
         List<String> recipeNames = new ArrayList<>();
 
@@ -39,11 +49,11 @@ public class ChooseRecipeState {
         return recipeNames;
     }
 
+    /**
+     * Setter which sets the list of recipe ideas.
+     * @param recipeIdeasList the new list of RecipeInformation objects to update state with
+     */
     public void setRecipeIdeasList(List<RecipeInformation> recipeIdeasList) {
         this.recipeIdeasList = recipeIdeasList;
     }
-
-
-
-
 }
--- src/use_case/back_to_search/BackToSearchInputBoundary.java
+++ src/use_case/back_to_search/BackToSearchInputBoundary.java
@@ -3,7 +3,7 @@ package use_case.back_to_search;
 public interface BackToSearchInputBoundary {
     /**
      * The back_to_search use case works immediately after the back button is pressed, and the
-     * use case does not require input data, so the interactor only must implment an execute method.
+     * use case does not require input data, so the interactor only must implement an execute method.
      */
     void execute();
 }
