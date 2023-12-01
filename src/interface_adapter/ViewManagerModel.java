25b67708e7d7b46ec4a36b7d8602cd91c3f6f8a5
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-11-30 20:29:48 -0500

    Add simple javadocs for view manager and its model.

--- src/interface_adapter/ViewManagerModel.java
+++ src/interface_adapter/ViewManagerModel.java
@@ -3,28 +3,48 @@ package interface_adapter;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * Manages the active view in SpoonfulOfIdeas.
+ * This class keeps track of the currently active view and notifies listeners when the active view changes.
+ */
 public class ViewManagerModel {
     private String activeViewName;
-
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
+    /**
+     * Gets the name of the currently active view.
+     * @return the name of the active view
+     */
     public String getActiveView() {
         return activeViewName;
     }
 
+    /**
+     * Sets the currently active view and notifies listeners of the change.
+     * @param activeView the name of the new active view
+     */
     public void setActiveView(String activeView) {
         this.activeViewName = activeView;
     }
 
-    // This is what the Signup Presenter will call to let the ViewModel know
+    // This is what the Presenter will call to let the ViewModel know
     // to alert the View
 
-    // first parameter changed
+    /**
+     * Fires a property change event indicating a change in the active view.
+     * This method should be called when the active view changes to notify all registered listeners.
+     */
     public void firePropertyChanged() {
         support.firePropertyChange("view", null, this.activeViewName);
         System.out.println("view manager model fire property change");
     }
 
+    /**
+     * Adds a property change listener to this view manager model.
+     * Allows for the registration of listeners that want to be notified of changes to the active view.
+     *
+     * @param listener the PropertyChangeListener to be added
+     */
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
     }
--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -7,12 +7,24 @@ import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+/**
+ * Manages the switching of views in the application's user interface.
+ * This class responds to property change events to switch between different views
+ * using a {@link CardLayout}.
+ */
 public class ViewManager implements PropertyChangeListener{
     private final CardLayout cardLayout;
     private final JPanel views;
 
     private ViewManagerModel viewManagerModel; //from the interface adapter
 
+    /**
+     * Constructs a ViewManager with specified panel, layout, and view manager model.
+     *
+     * @param views the panel containing the different views
+     * @param cardLayout the card layout to manage the views
+     * @param viewManagerModel the model managing view changes
+     */
     public ViewManager(JPanel views, CardLayout cardLayout, ViewManagerModel viewManagerModel) {
         this.views = views;
         this.cardLayout = cardLayout;
@@ -20,6 +32,10 @@ public class ViewManager implements PropertyChangeListener{
         this.viewManagerModel.addPropertyChangeListener(this);
     }
 
+    /**
+     * Responds to property change events to switch views.
+     * @param evt the property change event, typically indicating a view change
+     */
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         String viewModelName = (String) evt.getNewValue();
