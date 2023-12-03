d08d45dd7296a831366a42b1b81437596f2be4f4
shreya399 <shreya.dubey@mail.utoronto.ca>
2023-12-02 19:31:45 -0500

    made some changes so part of the view is also tested

--- test/use_case/choose_recipe/ChooseRecipeCoverageTest.java
+++ test/use_case/choose_recipe/ChooseRecipeCoverageTest.java
@@ -9,8 +9,10 @@ import interface_adapter.add_to_favourites.AddToFavouritesController;
 import interface_adapter.add_to_favourites.AddToFavouritesPresenter;
 import interface_adapter.back_to_choose.BackToChooseController;
 import interface_adapter.back_to_choose.BackToChoosePresenter;
+import interface_adapter.back_to_search.BackToSearchController;
 import interface_adapter.choose_recipe.ChooseRecipeController;
 import interface_adapter.choose_recipe.ChooseRecipePresenter;
+import interface_adapter.choose_recipe.ChooseRecipeState;
 import interface_adapter.choose_recipe.ChooseRecipeViewModel;
 import interface_adapter.nutrition_detail.NutritionDetailState;
 import interface_adapter.nutrition_detail.NutritionDetailViewModel;
@@ -22,7 +24,10 @@ import org.mockito.MockitoAnnotations;
 import use_case.add_to_favourites.*;
 import use_case.back_to_choose.BackToChooseInputBoundary;
 import use_case.back_to_choose.BackToChooseOutputData;
+import view.ChooseRecipeView;
 
+import javax.swing.*;
+import java.beans.PropertyChangeEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -117,4 +122,47 @@ public class ChooseRecipeCoverageTest {
         verify(mockInputBoundary).execute(any(ChooseRecipeInputData.class));
 
     }
+    @Test
+    public void ViewTest() {
+        // Arrange
+        ChooseRecipeController chooseRecipeController = mock(ChooseRecipeController.class);
+        BackToSearchController backToSearchController = mock(BackToSearchController.class);
+        ChooseRecipeViewModel chooseRecipeViewModel = mock(ChooseRecipeViewModel.class);
+
+        ChooseRecipeView chooseRecipeView = new ChooseRecipeView(chooseRecipeController, backToSearchController, chooseRecipeViewModel);
+
+        // Act
+        chooseRecipeView.back.doClick();
+
+        // Assert
+        verify(backToSearchController, times(1)).execute();
+    }
+
+   /* @Test
+    public void recipeButtonShouldInvokeChooseRecipeController() {
+        // Arrange
+        ChooseRecipeController chooseRecipeController = mock(ChooseRecipeController.class);
+        BackToSearchController backToSearchController = mock(BackToSearchController.class);
+        ChooseRecipeViewModel chooseRecipeViewModel = mock(ChooseRecipeViewModel.class);
+
+        ChooseRecipeView chooseRecipeView = new ChooseRecipeView(chooseRecipeController, backToSearchController, chooseRecipeViewModel);
+
+        // Setting up a sample state with one recipe
+        ChooseRecipeState sampleState = new ChooseRecipeState();
+        List<RecipeInformation> mockList = new ArrayList<>();
+        mockList.add(new RecipeInformation(1234, "Yum"));
+        sampleState.setRecipeIdeasList(mockList);
+
+        // Triggering propertyChange manually to simulate the change in state
+        chooseRecipeView.propertyChange(new PropertyChangeEvent(this, "state", null, sampleState));
+        JPanel recipePanel = (JPanel) chooseRecipeView.getComponent(0);
+        JButton recipeButton = (JButton) recipePanel.getComponent(0);  // Adjust the index based on your actual layout
+
+        // Act
+
+        recipeButton.doClick();
+
+        // Assert
+        verify(chooseRecipeController, times(1)).execute(any()); // Ensure execute is called with any argument
+    }*/
 }
